b43d72b942269debcad5d40a27bc234496ca9286
PiervitoCreanza <p.creanza@protonmail.com>
2020-10-22 19:05:57 +0200

    first commit

---
+++ assets/js/index.js
@@ -0,0 +1,32 @@
+const toggleBox = (n) => {
+    let p = document.getElementById(`toggleBox-${n}`)
+    let h = document.getElementById(`toggleBox-${n}-hidden`)
+    let c = document.getElementById(`toggleBox-${n}-circle`)
+    let i = document.getElementById(`toggleBox-${n}-icon`)
+
+    let isOpen = p.classList.contains("open")
+    console.log(isOpen)
+    p.classList.toggle("text-light")
+    p.classList.toggle("open")    
+    p.classList.toggle("bg-dark")
+    h.classList.toggle("fade-out")
+    if (isOpen) {
+        console.log(1)
+        setTimeout(() => {
+            h.classList.toggle("hidden")
+            p.classList.toggle("p-4_5")
+        }, 400)
+    } else {
+        h.classList.toggle("hidden")
+        p.classList.toggle("p-4_5")
+        console.log(2)
+    }
+    
+    
+    c.classList.toggle("dark-circle")
+    if (i.src.includes("assets/icns/arrow-down-white.svg")) {
+        i.src = "assets/icns/arrow-down.svg"
+    } else {
+        i.src = "assets/icns/arrow-down-white.svg"
+    }
+}
