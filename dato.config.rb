1d3ca46928c6ca97aed0fa0f8bc14b12b01d26c4
PiervitoCreanza <p.creanza@protonmail.com>
2022-10-22 15:42:40 +0200

    Add domande from DatoCMS

--- dato.config.rb
+++ dato.config.rb
@@ -9,16 +9,42 @@ create_data_file("_data/incontri.json", :json,
 
     listaIncontri: dato.tipi_di_incontris.map do |tipoIncontro|
         {
-            titolo: tipoIncontro.titolo,
-            sottotitolo: tipoIncontro.sottotitolo,
+            titolo: {
+                titolo_rosso: tipoIncontro.titolo_rosso,
+                titolo_nero: tipoIncontro.titolo_nero,
+            },
+            sottotitolo: {
+                sottotitolo_nero: tipoIncontro.sottotitolo,
+                sottotitolo_rosso: tipoIncontro.sottotitolo_rosso
+            },
             date: dato.incontris.select {|incontro| incontro.tipo_di_incontro.titolo == tipoIncontro.titolo}.map do |incontro| 
                 {
-                    giorno: incontro.giorno,
-                    mese: incontro.mese,
                     orario: "Dalle " + incontro.ora_inizio + " alle " + incontro.ora_fine,
                     url: incontro.url,
                     pulsante: incontro.testo_pulsante,
                     data: incontro.data
+                }
+            end
+        }
+        end
+)
+
+create_data_file("_data/domande.json", :json, 
+
+    sezione1: {
+        titolo: "<span class='text-red'>Hai una</span> domanda?",
+        sottotitolo: "Le domande frequenti che possono aiutarti nella fase di scelta e di iscrizione.",
+        pulsanteRosso: "Contattaci",
+        pulsante: "Vai alle domande frequenti"
+    },
+
+    domande: dato.gruppi_di_domandes.map do |argomento|
+        {
+            argomento: argomento.nome,
+            domande: dato.domandes.select {|domanda| domanda.argomento.nome == argomento.nome}.map do |domanda| 
+                {
+                    domanda: domanda.domanda,
+                    risposta: domanda.risposta
                 }
             end
         }
